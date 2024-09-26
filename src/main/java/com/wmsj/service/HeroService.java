793eda8dd215195ae011f64810eae7769d2b9b22
qilongzz <qilongzz@163.com>
2024-09-26 17:51:04 +0800

    commit for opgg

--- src/main/java/com/wmsj/service/HeroService.java
+++ src/main/java/com/wmsj/service/HeroService.java
@@ -4,4 +4,12 @@ import com.wmsj.common.service.BaseService;
 import com.wmsj.entity.Hero;
 
 public interface HeroService extends BaseService<Hero> {
+    //增
+    public int insertHero(Hero hero) ;
+    //删
+    public int deleteHero(int id) ;
+    //改
+    public int updateHero(Hero hero);
+    //查
+    public Hero getHeroById(int id);
 }
--- src/main/java/com/wmsj/service/impl/HeroServiceImpl.java
+++ src/main/java/com/wmsj/service/impl/HeroServiceImpl.java
@@ -5,10 +5,29 @@ import com.wmsj.dao.HeroDao;
 import com.wmsj.entity.Hero;
 import com.wmsj.service.HeroService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class HeroServiceImpl extends BaseServiceImpl<HeroDao, Hero> implements HeroService {
+    @Autowired
+    private HeroDao heroDao;
 
+    //增
+    public int insertHero(Hero hero) {
+        return heroDao.insert(hero);
+    }
+    //删
+    public int deleteHero(int id) {
+        return heroDao.deleteById(id);
+    }
+    //改
+    public int updateHero(Hero hero) {
+        return heroDao.updateById(hero);
+    }
+    //查
+    public Hero getHeroById(int id){
+        return heroDao.selectById(id);
+    }
 }
