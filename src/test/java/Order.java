d7a5e08ce1ae1760a395211f37a5186db283de3c
qilongzz <qilongzz@163.com>
2024-10-10 17:30:23 +0800

    commit for opgg

---
+++ src/test/java/Order.java
@@ -0,0 +1,13 @@
+public class Order {
+
+    String showStatus;
+
+    public Order(String showStatus) {
+        this.showStatus = showStatus;
+    }
+
+    public String getShowStatus() {
+        return showStatus;
+    }
+
+}
